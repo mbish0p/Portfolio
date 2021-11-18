f4d3f1e7f03a34c8eebd2352e0905db70ab5f648
mateusz <biskup.mateusz1997@wp.pl>
2021-11-18 19:15:42 +0100

    Finish mobile hero section

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 //hooks
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 //assets
 import stars from './images/hero/stars.png';
@@ -19,10 +19,11 @@ const Hero = () => {
     const behindRef = useRef<HTMLImageElement>(null);
     const textRef = useRef<HTMLHeadingElement>(null);
 
+    const [isActive, setIsActive] = useState(false);
+
 
     useEffect(() => {
         const textRight = textRef.current!.offsetLeft;
-        console.log(textRight, 'lama');
         window.addEventListener('scroll', function () {
             let offsetY = window.scrollY;
             starsRef.current!.style.left = offsetY * 0.25 + 'px';
@@ -35,18 +36,35 @@ const Hero = () => {
     return (
         <div className='hero--container'>
             <div className='head--container'>
-                <a href='#' className='logo'>Logo</a>
+                <a href='#' className='header--h2 uppercase'>Logo</a>
                 <ul className='nav--items'>
-                    <li className='item'><a href='#'>Home</a></li>
-                    <li className='item'><a href='#'>About</a></li>
-                    <li className='item'><a href='#'>Work</a></li>
-                    <li className='item'><a href='#'>Contact</a></li>
+                    <li className='item'><a href='#' className='paragraph--component'>Home</a></li>
+                    <li className='item'><a href='#' className='paragraph--component'>About</a></li>
+                    <li className='item'><a href='#' className='paragraph--component'>Work</a></li>
+                    <li className='item'><a href='#' className='paragraph--component'>Contact</a></li>
                 </ul>
+
+                <div className={`mobile--menu ${isActive ? '-active' : ''}`}>
+                    <div className={`toggle ${isActive ? '-active' : ''}`} onClick={() => setIsActive(!isActive)}>
+                    </div>
+                    <div className='item -home' style={{ '--i': '1' } as React.CSSProperties}>
+                        <a href='#' className='link'></a>
+                    </div>
+                    <div className='item -about' style={{ '--i': '2' } as React.CSSProperties}>
+                        <a href='#' className='link'></a>
+                    </div>
+                    <div className='item -work' style={{ '--i': '3' } as React.CSSProperties}>
+                        <a href='#' className='link'></a>
+                    </div>
+                    <div className='item -contact' style={{ '--i': '4' } as React.CSSProperties}>
+                        <a href='#' className='link'></a>
+                    </div>
+                </div>
             </div>
             <section className='paralax--animation'>
                 <img ref={starsRef} src={stars} id='stars' alt='stars' className='img' />
                 <img ref={moonRef} src={moon} id='moon' alt='moon' className='img -moon' />
-                <h2 ref={textRef} className='text'>Portfolio</h2>
+                <h2 ref={textRef} className='header--h2 text'>Portfolio</h2>
                 <img ref={behindRef} src={moutains_behind} id='moutains_behind' alt='moutains_behind' className='img' />
                 <img src={moutains_front} id='moutains_front' alt='moutains_front' className='img -front' />
             </section>

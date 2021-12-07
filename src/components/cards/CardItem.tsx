b381d74bd7984dd0b7c0ac7b12ce0f7b621f7c3e
mateusz <biskup.mateusz1997@wp.pl>
2021-12-07 19:25:07 +0100

    Fix cards physics

--- src/components/cards/CardItem.tsx
+++ src/components/cards/CardItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 //libararies
 import { animated, useSpring, to } from 'react-spring';
@@ -8,61 +8,76 @@ import { useGesture } from '@use-gesture/react';
 import { Card } from '../../types/index';
 
 type Props = {
-        item: Card;
+    item: Card;
 };
 
 const CardItem = ({ item }: Props) => {
 
-        const calcX = (y: number, ly: number) => -(y - ly - window.innerHeight / 2) / 40;
-        const calcY = (x: number, lx: number) => (x - lx - window.innerWidth / 2) / 40;
+    const ref = useRef<HTMLDivElement>(null);
 
-        const [{ x, y, rotateX, rotateY, rotateZ, zoom, scale }, api] = useSpring(
-                () => ({
-                        rotateX: 0,
-                        rotateY: 0,
-                        rotateZ: 0,
-                        scale: 1,
-                        zoom: 0,
-                        x: 0,
-                        y: 0,
-                        config: { mass: 5, tension: 350, friction: 40 },
-                })
-        );
+    const calcX = (y: number, ly: number) => {
+        if (ref.current) {
+            const position = ref.current.getBoundingClientRect();
+            const elCenter = (position.y + ref.current.clientHeight / 2);
+            return -(y - elCenter) / 40;
+        }
+    };
+    const calcY = (x: number, lx: number) => {
+        if (ref.current) {
+            const position = ref.current.getBoundingClientRect();
+            const elCenter = (position.x + ref.current.clientWidth / 2);
+            return (x - elCenter) / 40;
+        }
+    };
 
-        const bind = useGesture({
-                onMove: (state) => {
-                        const { xy: [px, py], dragging } = state;
-                        !dragging &&
-                                api({
-                                        rotateX: calcX(py, y.get()),
-                                        rotateY: calcY(px, x.get()),
-                                        scale: 1.1,
-                                });
-                },
-                onHover: (state) => {
-                        !state.hovering && api({ rotateX: 0, rotateY: 0, scale: 1 });
-                }
-        });
+    const [{ x, y, rotateX, rotateY, rotateZ, zoom, scale }, api] = useSpring(
+        () => ({
+            rotateX: 0,
+            rotateY: 0,
+            rotateZ: 0,
+            scale: 1,
+            zoom: 0,
+            x: 0,
+            y: 0,
+            config: { mass: 5, tension: 350, friction: 40 },
+        })
+    );
 
-        return (
-                <animated.div
-                        {...bind()}
-                        className='card'
-                        style={{
-                                transform: 'perspective(600px)',
-                                x,
-                                y,
-                                scale: to([scale, zoom], (s, z) => s + z),
-                                rotateX,
-                                rotateY,
-                                rotateZ,
-                        }}
-                >
-                        <div className='card--image' style={{ backgroundImage: `url(${item.avatar})` }} />
-                        <p className='paragraph--component -big text-center mb-4'>{item.name}</p>
-                        <p className='paragraph--component text-center'>{item.description}</p>
-                </animated.div>
-        );
+    const bind = useGesture({
+        onMove: (state) => {
+            const { xy: [px, py], dragging } = state;
+            !dragging &&
+                api({
+                    rotateX: calcX(py, y.get()),
+                    rotateY: calcY(px, x.get()),
+                    scale: 1.05,
+                });
+        },
+        onHover: (state) => {
+            !state.hovering && api({ rotateX: 0, rotateY: 0, scale: 1 });
+        }
+    });
+
+    return (
+        <animated.div
+            {...bind()}
+            className='card'
+            style={{
+                transform: 'perspective(600px)',
+                x,
+                y,
+                scale: to([scale, zoom], (s, z) => s + z),
+                rotateX,
+                rotateY,
+                rotateZ,
+            }}
+            ref={ref}
+        >
+            <div className='card--image' style={{ backgroundImage: `url(${item.avatar})` }} />
+            <p className='paragraph--component -big text-center mb-4'>{item.name}</p>
+            <p className='paragraph--component text-center'>{item.description}</p>
+        </animated.div>
+    );
 };
 
 export default CardItem;

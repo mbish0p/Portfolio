f60b10cc265ad1850f76b815b6f3f9b7a93ae14e
mbish0p <biskup.mateusz1997@wp.pl>
2021-12-25 17:53:01 +0100

    Create tamplate for about section

--- src/components/homepage/App.tsx
+++ src/components/homepage/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import Slider from './Slider';
-import Hero from './Hero';
+import React from "react";
+
+//components
+import Slider from "./Slider";
+import Hero from "./Hero";
+import About from "./About";
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <div className="sliders--container">
-        <div className='container'>
-          <h2 className='header--h2 pb-20 text-left'>Przygotowane prace</h2>
+        <div className="container">
+          <h2 className="header--h2 pb-20 text-left">Przygotowane prace</h2>
         </div>
+        <About />
         <Slider />
       </div>
     </div>
